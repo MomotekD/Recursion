968634773cb9c46bd6d3a42d72699bae7d2303b1
MomotekD <86006101+MomotekD@users.noreply.github.com>
2025-05-15 13:17:22 +0200

    Add: ;

--- fibs.js
+++ fibs.js
@@ -1,30 +1,30 @@
 function fibs(n){
-  const startingArr = [0,1]
+  const startingArr = [0,1];
   if (n <= 1){
-    return [0]
+    return [0];
   }
   for(let i = 2; i <= n-1; i++){
-    startingArr.push(startingArr[startingArr.length - 1] + startingArr[startingArr.length - 2])
+    startingArr.push(startingArr[startingArr.length - 1] + startingArr[startingArr.length - 2]);
   }
-  return startingArr
+  return startingArr;
 }
 
-console.log(fibs(10))
+console.log(fibs(10));
 
-function fibsRec(n) {
+function fibsRec(n){
     if(n === 0){
-        return []
+        return [];
     }
     if(n === 1){
-        return [0]
+        return [0];
     }
    if(n === 2){
-        return [0, 1]
+        return [0, 1];
    }
-   let prev = fibsRec(n - 1)
-   let nextNumber = prev[prev.length - 1] + prev[prev.length - 2]
-   prev.push(nextNumber)
-   return prev
+   let prev = fibsRec(n - 1);
+   let nextNumber = prev[prev.length - 1] + prev[prev.length - 2];
+   prev.push(nextNumber);
+   return prev;
 }
 
-console.log(fibsRec(0))
+console.log(fibsRec(0));
